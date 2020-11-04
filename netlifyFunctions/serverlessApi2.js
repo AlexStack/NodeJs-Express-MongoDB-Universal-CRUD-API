91a26de068d57c5c89eb84f0314569c78966fdef
Alex <alex@10168.com>
2020-11-04 21:11:51 +1300

    Tidy up pending delete files

---
+++ netlifyFunctions/serverlessApi2.js
@@ -0,0 +1,3 @@
+const { app, serverless } = require("../app/express/universal.app");
+
+module.exports.handler = serverless(app);
--- netlifyFunctions/serverlessapi.js
+++
@@ -1,3 +0,0 @@
-const { app, serverless } = require("./app/express/universal.app");
-
-module.exports.handler = serverless(app);
