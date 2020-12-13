d8a426f0d88f606e8b8ed53a2daec0429fd43c26
Alex Zeng <alex@10168.com>
2020-12-14 12:01:01 +1300

    Add index() by aggregate, support _embed, _expand, _like ...

--- app/controllers/universal.controller.js
+++ app/controllers/universal.controller.js
@@ -43,7 +43,7 @@ exports.store = (req, res) => {
 };
 
 // Retrieve all universals from the database.
-exports.index = (req, res) => {
+exports.indexByFind = (req, res) => {
 
   // Universal = db[req.url.replace('/' + API_CONFIG.API_BASE, '')];
   const Universal = getUniversalDb(req, res);
@@ -177,10 +177,227 @@ exports.index = (req, res) => {
   });
 };
 
+
+// Retrieve all universals from the database by aggregate
+exports.index = (req, res) => {
+
+  // Universal = db[req.url.replace('/' + API_CONFIG.API_BASE, '')];
+  const Universal = getUniversalDb(req, res);
+
+  console.log('===== query', req.query)
+  const apiSchema = getApiSchema(req, res);
+  // console.log('apiSchema=', apiSchema);
+
+  let condition = {};
+  let tempVar;
+  for (paramName in req.query) {
+    let paramValue = req.query[paramName];
+
+    // Add _like to filter %keyword%, e.g. name_like=ca
+    // support simple regex syntax
+    // e.g.name_like=^v  name_like=n$ name_like=jack|alex  name_like=or?n
+    if (paramName.indexOf('_like') != -1) {
+      paramName = paramName.replace('_like', '');
+      paramValue = '%' + paramValue + '%';
+    }
+
+    // Add _gte or _lte for getting a range(cannot use together yet)
+    // , e.g. age_lte=60  age_gte=18
+    if (paramName.indexOf('_gte') != -1) {
+      paramName = paramName.replace('_gte', '');
+      paramValue = '>' + paramValue;
+    }
+    if (paramName.indexOf('_lte') != -1) {
+      paramName = paramName.replace('_lte', '');
+      paramValue = '<' + paramValue;
+    }
+
+    if (paramName in apiSchema.schema) {
+      if (apiSchema.schema[paramName] == String || ("type" in apiSchema.schema[paramName] && apiSchema.schema[paramName].type == String)) {
+        if (paramValue.indexOf('==') === 0) {
+          // e.g. title===Cat 
+          // case sensitive equal match, better to set up index
+          condition[paramName] = paramValue.replace('==', '');
+        } else if (paramValue.substr(0, 1) == '%' && paramValue.substr(-1) == '%') {
+          // e.g. title=%ca%
+          // case insensitive full text match, will not use index, slow
+          condition[paramName] = { $regex: new RegExp(paramValue.replace(/%/g, '')), $options: "i" };
+        } else if (paramValue.indexOf('i=') === 0) {
+          // e.g. title=i=cat
+          // case insensitive equal match, may not use the index
+          // https://docs.mongodb.com/manual/reference/operator/query/regex/
+          condition[paramName] = { $regex: new RegExp("^" + paramValue.replace('i=', '').toLowerCase() + "$", "i") };
+        } else {
+          // e.g. title=cat, same as title===cat
+          // case sensitive equal match, better to set up index
+          condition[paramName] = paramValue.replace('==', '');
+        }
+
+      } else if (apiSchema.schema[paramName] == Number || ("type" in apiSchema.schema[paramName] && apiSchema.schema[paramName].type == Number)) {
+        if (paramValue.indexOf('>') === 0) {
+          // e.g. age=>18
+          tempVar = Number(paramValue.replace('>', ''));
+          if (!isNaN(tempVar)) {
+            condition[paramName] = { $gte: tempVar };
+          }
+        } else if (paramValue.indexOf('<') === 0) {
+          // e.g. age=<18
+          tempVar = Number(paramValue.replace('<', ''));
+          if (!isNaN(tempVar)) {
+            condition[paramName] = { $lte: tempVar };
+          }
+        } else if (!isNaN(paramValue)) {
+          // e.g. age=18
+          condition[paramName] = { $eq: Number(paramValue) };
+        }
+      }
+    }
+    // console.log(paramName, paramValue)
+  }
+
+  // find multiple ids, e.g.  ?id=1&id=2&id=3 or ?id=1,2,3,4,5
+  if (req.query.id || req.query._id) {
+    const originId = req.query.id ? req.query.id : req.query._id;
+    const idAry = (typeof originId == 'string') ? originId.split(',') : originId;
+    let objIdAry = [];
+    idAry.map(idStr => {
+      objIdAry.push(db.mongoose.Types.ObjectId(idStr));
+    })
+    condition["_id"] = { "$in": objIdAry };
+  }
+
+  // full text search
+  if (req.query.q && req.query.q.trim() != '') {
+    return this.search(req, res);
+  }
+
+  console.log('find query condition:', condition);
+
+  // Add _sort and _order (ascending order by default)
+  let defaultSort = {};
+  if (req.query._sort && req.query._order) {
+    if (req.query._sort in apiSchema.schema) {
+      defaultSort[req.query._sort] = (req.query._order.toUpperCase() == 'DESC') ? -1 : 1;
+    }
+  }
+  if (Object.keys(defaultSort).length === 0) {
+    defaultSort = { _id: -1 };
+  }
+
+  // Add _start and _end or _limit 
+  let defaultSkip = 0;
+  let defaultLimit = 2000;
+  if (req.query._start && req.query._end) {
+    defaultSkip = parseInt(req.query._start);
+    defaultLimit = req.query._limit ? parseInt(req.query._limit) : (parseInt(req.query._end) - parseInt(req.query._start));
+  }
+
+
+  // let query = Universal.find(condition).sort(defaultSort);
+
+  // // only display specific fields or exclude some fields
+  // if (apiSchema.selectFields && apiSchema.selectFields.length > 0) {
+  //   query.select(apiSchema.selectFields);
+  // }
+
+  // if (defaultSkip > 0) {
+  //   query.skip(defaultSkip);
+  // }
+  // if (defaultLimit > 0) {
+  //   query.limit(defaultLimit);
+  // }
+  // query.then(async (data) => {
+  //   const totalNumber = await Universal.countDocuments(condition);
+  //   console.log('defaultSort', defaultSort, totalNumber);
+  //   // const totalNumber = data.length;
+  //   res.set("Access-Control-Expose-Headers", "X-Total-Count");
+  //   res.set("x-total-count", totalNumber);
+  //   res.send(data);
+  // }).catch((err) => {
+  //   res.status(500).send({
+  //     message:
+  //       err.message || "Some error occurred while retrieving items.",
+  //   });
+  // });
+
+  let pipelineOperators = [
+    {
+      // '$match': { _id: db.mongoose.Types.ObjectId(id) }
+      '$match': condition,
+    },
+    {
+      '$sort': defaultSort,
+    },
+    {
+      '$limit': defaultLimit,
+    }
+  ];
+
+  // To include children resources, add _embed
+  if (req.query._embed) {
+    req.query._embed.split(',').map(tableName => {
+      const embedSchema = API_CONFIG.API_SCHEMAS.find(apiSchema => apiSchema.collectionName == tableName);;
+      if (embedSchema) {
+        // console.log('tableName embedSchema', embedSchema)
+        pipelineOperators.push(getChildrenLookupOperator(null, tableName, apiSchema, embedSchema));
+      } else {
+        console.log('tableName schema not defined', tableName)
+      }
+    })
+  }
+
+  // To include parent resource, add _expand
+  if (req.query._expand) {
+    req.query._expand.split(',').map(tableName => {
+      const expandTable = tableName.split('|');
+      const singularTableName = expandTable[0];
+      const foreignField = expandTable[1] ? expandTable[1] : singularTableName + 'Id';
+      const pluralTableName = getPluralName(singularTableName);
+      const expandSchema = API_CONFIG.API_SCHEMAS.find(apiSchema => apiSchema.collectionName == pluralTableName);
+      if (expandSchema) {
+        // console.log('tableName expandSchema', expandSchema)
+        pipelineOperators.push(getParentLookupOperator(foreignField, singularTableName, pluralTableName, expandSchema));
+
+        pipelineOperators.push({ $unwind: { path: "$" + singularTableName, "preserveNullAndEmptyArrays": true } });
+      } else {
+        console.log('tableName schema not defined', singularTableName, pluralTableName)
+      }
+
+    })
+  }
+
+  // pipelineOperators.push(
+  //   { '$match': { "$pet.species": "Cat" } }
+  // )
+
+  // combine pre-defined pipeline from api.config.js
+  if (apiSchema.aggregatePipeline) {
+    pipelineOperators = [...pipelineOperators, ...apiSchema.aggregatePipeline]
+  }
+
+  console.log('===exports.show pipelineOperators', pipelineOperators);
+
+  let query = Universal.aggregate(pipelineOperators).exec((err, data) => {
+    if (err) {
+      return console.log('aggregate error', err)
+    }
+    console.log('aggregate data.length', data.length)
+    if (!data || data.length == 0)
+      res.status(404).send({ message: "Not found the item with id ", data });
+    else {
+      res.send(data);
+    };
+  });
+
+  // console.log(query)
+  return query;
+};
+
+
 const getTableId = (tableName) => {
   let tableId = tableName + 'Id';
   if (tableName.slice(-3) == 'ies') {
-    tableId = tableName.slice(0, -3) + 'Id';
+    tableId = tableName.slice(0, -3) + 'yId';
   } else if (tableName.slice(-2) == 'es') {
     tableId = tableName.slice(0, -2) + 'Id';
   } else if (tableName.slice(-1) == 's') {
@@ -209,17 +426,23 @@ const getChildrenLookupOperator = (id, tableName, apiSchema, embedSchema) => {
   const foreignField = getTableId(apiSchema.collectionName);
   let pipeline = embedSchema.aggregatePipeline ? embedSchema.aggregatePipeline : [];
   let match = {};
-  match[foreignField] = id;
+  if (id) {
+    // for show()
+    match[foreignField] = id;
+  } else {
+    // for index() 
+    match['$expr'] = { "$eq": ["$" + foreignField, "$$strId"] };
+  }
   pipeline = [{ '$match': match }, ...pipeline];
   const lookupOperator = {
     '$lookup': {
       'from': tableName,
-      // 'localField': '_id',
-      // 'foreignField': foreignField,
+      "let": { "strId": "$id" }, // maybe $_id in some case
       'as': tableName,
       'pipeline': pipeline
     }
   }
+  console.log('lookupOperator pipeline.match', match);
   return lookupOperator;
 }
 
@@ -237,7 +460,14 @@ const getParentLookupOperator = (foreignField, singularTableName, pluralTableNam
     ...[
       {
         $addFields: {
-          objId: { "$toObjectId": "$$strId" }
+          objId: {
+            $convert: {
+              input: "$$strId",
+              to: "objectId",
+              onError: 0
+            }
+            // "$toObjectId": "$$strId"
+          }
         }
       },
       { '$match': match }
