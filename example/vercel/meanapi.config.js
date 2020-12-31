45c0491561e0fa72834e837fb2d0eb256c25c0b2
Alex Zeng <alex@10168.com>
2020-12-31 18:26:44 +1300

    Test vercel example

---
+++ example/vercel/meanapi.config.js
@@ -0,0 +1,50 @@
+module.exports.DB = process.env.DB || 'please-set-database-connect-uri-first';
+module.exports.API_BASE = process.env.API_BASE || 'api/';
+module.exports.PORT = process.env.PORT || '8080';
+module.exports.CORS_ORIGIN = process.env.CORS_ORIGIN || [/localhost/, /\.test$/];
+module.exports.IS_SERVERLESS = !!(process.env.LAMBDA_TASK_ROOT || process.env.AWS_LAMBDA_FUNCTION_NAME);
+
+module.exports.API_SCHEMAS = [
+    {
+        "apiRoute": "test1",
+        "collectionName": "test1",
+        "schema": {
+            name: String,
+            description: String,
+            type: String,
+            gender: String,
+            dob: Date,
+            userId: { type: Number, required: [true, 'userId is required.'] }
+        },
+        "mongooseOption": { timestamps: true, strict: false },
+        "searchFields": ["name", "description", "type"],
+    },
+    {
+        "apiRoute": "test2",
+        "collectionName": "test2",
+        "schema": {
+            title: {
+                type: String,
+                required: [true, 'Title is required.']
+            },
+            description: String,
+            age: {
+                type: Number,
+                min: [18, 'Too young'],
+                max: 80
+            }
+        },
+        "mongooseOption": { timestamps: true, strict: true },
+        "searchFields": ["description", "title"],
+    },
+    {
+        "apiRoute": "test3",
+        "collectionName": "test3",
+        "schema": {
+            address: String,
+            description: String
+        },
+        "mongooseOption": { timestamps: false, strict: false },
+        "searchFields": ["address"],
+    }
+];
---
+++ example/vercel/vercel.js
@@ -0,0 +1,6 @@
+const { app, serverless, API_CONFIG } = require("universal-mean-api");
+
+// module.exports = serverless(app);
+app.listen(API_CONFIG.PORT, () => {
+    console.log(`Server is running on port ${API_CONFIG.PORT}.`);
+});
